dc10c11f398501de361e0899f50f3d1d8c143cd5
yuhao.ju <yuhao.ju@qunar.com>
2017-02-17 20:28:39 +0800

    feat: 添加 runtime 和 happypack

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 'use strict';
 
+var path = require('path');
+var HappyPack = require('happypack');
+
 exports.config = function (options, cwd) {
     var baseConfig = this.config;
     extend(true, baseConfig, {
         module: {
-            loaders: baseConfig.module.loaders.concat ([{
-                test: /\.js|jsx$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: [
-                        'es2015',
-                        'es2017',
-                        'react',
-                        'stage-0',
-                        'stage-1',
-                        'stage-2',
-                    ]
-                }
+            loaders: baseConfig.module.loaders.concat([{
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loaders: ['happypack/loader']
             }])
-        }
+        },
+        plugins: baseConfig.plugins.concat([
+            new HappyPack({
+                loaders: [
+                    {
+                        loader: require.resolve('babel-loader'),
+                        test: /\.(js|jsx)$/,
+                        exclude: /node_modules/,
+                        query: {
+                            cacheDirectory: true,
+                            presets: [
+                                'es2015',
+                                'es2017',
+                                'react',
+                                'stage-0',
+                                'stage-1',
+                                'stage-2',
+                            ],
+                            plugins: ['transform-runtime']
+                        }
+                    }
+                ],
+                threads: 4,
+                verbose: false,
+                cacheContext: {
+                    env: process.env.NODE_ENV
+                },
+                tempDir: path.join(cwd, 'node_modules/.happypack'),
+                cachePath: path.join(cwd, 'node_modules/.happypack/cache--[id].json')
+            })
+        ])
     });
-
-    this.packCallbacks.push(function (opt, stats) {
-        // 构建钩子
-    });
-
-    // 自定义命令
-    // this.commands.push({
-    //     name: 'setup',
-    //     module: require('./commands/setup.js')
-    // });
 };
