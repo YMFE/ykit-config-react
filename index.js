13aacc5c67aea551eac52e4cbb8cae11b3fe9886
yuhao.ju <yuhao.ju@qunar.com>
2017-03-31 15:39:54 +0800

    feat(config): 提供更改配置的方法

--- index.js
+++ index.js
@@ -5,6 +5,36 @@ var HappyPack = require('happypack');
 
 exports.config = function (options, cwd) {
     var baseConfig = this.config;
+    var happyPackConfig = {
+        loaders: [
+            {
+                loader: require.resolve('babel-loader'),
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: [
+                        'es2015',
+                        'es2017',
+                        'react',
+                        'stage-0',
+                        'stage-1',
+                        'stage-2',
+                    ],
+                    plugins: ['transform-runtime']
+                }
+            }
+        ],
+        threads: 4,
+        verbose: false,
+        cacheContext: {
+            env: process.env.NODE_ENV
+        },
+        tempDir: path.join(cwd, 'node_modules/.happypack'),
+        cachePath: path.join(cwd, 'node_modules/.happypack/cache--[id].json')
+    };
+    happyPackConfig = options.modifyReactConfig(happyPackConfig);
+
     extend(true, baseConfig, {
         module: {
             loaders: baseConfig.module.loaders.concat([{
@@ -14,33 +44,11 @@ exports.config = function (options, cwd) {
             }])
         },
         plugins: baseConfig.plugins.concat([
-            new HappyPack({
-                loaders: [
-                    {
-                        loader: require.resolve('babel-loader'),
-                        test: /\.(js|jsx)$/,
-                        exclude: /node_modules/,
-                        query: {
-                            cacheDirectory: true,
-                            presets: [
-                                'es2015',
-                                'es2017',
-                                'react',
-                                'stage-0',
-                                'stage-1',
-                                'stage-2',
-                            ],
-                            plugins: ['transform-runtime']
-                        }
-                    }
-                ],
-                threads: 4,
-                verbose: false,
-                cacheContext: {
-                    env: process.env.NODE_ENV
-                },
-                tempDir: path.join(cwd, 'node_modules/.happypack'),
-                cachePath: path.join(cwd, 'node_modules/.happypack/cache--[id].json')
+            new HappyPack(happyPackConfig),
+            new this.webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: JSON.stringify(this.env === 'prd' ? 'production' : 'dev')
+                }
             })
         ])
     });
