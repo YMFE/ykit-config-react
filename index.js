544157326fc599c8dd5fbcd377cded1022ae30fe
祝鑫奔 <rolandreed.cn@gmail.com>
2018-06-14 14:23:20 +0800

    feat: support ykit@4

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var path = require('path');
-var HappyPack = require('happypack');
-var es6Config = require('ykit-config-es6');
+const path = require('path');
+const es6Config = require('ykit-config-es6');
 
 exports.config = function(options, cwd) {
-    var originModifyQuery = options.modifyQuery;
+    const originModifyQuery = options.modifyQuery;
 
     options.modifyQuery = function(defaultQuery) {
         defaultQuery.presets.push('react');
@@ -19,32 +18,18 @@ exports.config = function(options, cwd) {
 
     es6Config.config.call(this, options, cwd);
 
-    extend(true, this.config, {
-        plugins: this.config.plugins.concat([
-            new this.webpack.DefinePlugin({
-                'process.env': {
-                    NODE_ENV: JSON.stringify(
-                        this.env === 'prd' || this.env === 'beta'
-                            ? 'production'
-                            : 'dev'
-                    )
-                }
-            })
-        ])
-    });
-
     if (options.ie8) {
         var pkg = require(path.join(cwd, 'package.json'));
         var reactVer = pkg.dependencies.react;
         var reactDomVer = pkg.dependencies['react-dom'];
 
         if (!reactVer || reactVer.search(/\^?0\./) === -1) {
-            console.log('兼容 ie8 需要手动安装 react@0.14.8');
+            console.log('兼容 ie8 需要手动安装 react@0.14.8'); // eslint-disable-line
             process.exit(0);
         }
 
         if (!reactDomVer || reactDomVer.search(/\^?0\./) === -1) {
-            console.log('兼容 ie8 需要手动安装 react-dom@0.14.8');
+            console.log('兼容 ie8 需要手动安装 react-dom@0.14.8'); // eslint-disable-line
             process.exit(0);
         }
 
